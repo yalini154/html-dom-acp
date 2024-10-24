0273d4ab681a440fbdca23256372e8f8bd7d98c5
yalini154 <lexmazhilan@gmail.com>
2024-10-24 21:09:27 +0400

    html dom

---
+++ script.js
@@ -0,0 +1,10 @@
+function muti(){
+    var a = 14;
+    var b = 7;
+    document.getElementById("power").innerHTML=a*b
+}
+function muti2(){
+    var c = 6;
+    var d = 9;
+    document.getElementById("power").innerHTML=c*d
+}
